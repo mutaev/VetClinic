141c9ea244945a0ab19081210deb1a889bc9fe81
eldar mutaev <eldarmutaev@inbox.ru>
2023-06-14 01:41:32 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -6,25 +6,6 @@ $(function () {
     $('body').toggleClass('fixed-page');
     });
 
-	$('.menu-burger__header2').click(function(){
-    $('.menu-burger__header2').toggleClass('open-menu2');
-    $('.header__nav2').toggleClass('open-menu2');
-    $('body').toggleClass('fixed-page');
-    });
-
-	$('.top__menu-btn').click(function(){
-    $('.top__menu-btn').toggleClass('open-menu3');
-    $('.top__menu').toggleClass('open-menu3');
-    $('body').toggleClass('fixed-page');
-    });
-
-	$('.top__menu-btn2').click(function(){
-    $('.top__menu-btn2').toggleClass('open-menu4');
-    $('.top__menu2').toggleClass('open-menu4');
-    $('body').toggleClass('fixed-page');
-    });
-
-
 
 
 })
